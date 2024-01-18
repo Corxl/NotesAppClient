83ff2a8add6e2f6c78895fb7c2dd65fcedc49b9a
BuildTools <unconfigured@null.spigotmc.org>
2024-01-17 16:49:25 -0800

    added session authentication example in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
- import 'primereact/resources/themes/lara-light-cyan/theme.css';
-import { useState } from 'react';
+ import axios from 'axios';
+import 'primereact/resources/themes/lara-light-cyan/theme.css';
+import { useEffect, useState } from 'react';
 import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
 import { AccountPage } from './components/Account';
@@ -12,8 +13,38 @@ import { LoginContext } from './context/LoginContext.tsx';
 function App() { 
 
 	const [isLoggedIn, setIsLoggedIn] = useState(true); 
+	axios.defaults.withCredentials = true; 
+	async function handleLogin() {
+		await axios.post('http://localhost:3001/users/login',
+			{ 
+				username: 'test',
+				password: 'test', 
+			}).then((res) => {
+			console.log(res);
+			// setIsLoggedIn(true);
+		}
+		).catch((err) => {
+			console.log(err);
+			// setIsLoggedIn(false);
+		});
+	}
+	async function handleProtected() {
+		await axios.get('http://localhost:3001/users/protected').then((res) => {
+			console.log(res);
+			// setIsLoggedIn(true);
+		}
+		).catch((err) => {
+			console.log(err);
+			// setIsLoggedIn(false);
+		});
+	}
+	useEffect(() => {
+		handleLogin();
+		handleProtected();
+	}, []);
 
-  return (
+
+    return (
 		<LoginContext.Provider value={{isLoggedIn, setIsLoggedIn}}>
 			<HashRouter>
 				<div className="App">
